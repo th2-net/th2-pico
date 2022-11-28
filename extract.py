3306b64420059d13c0062179f8cb70005041b264
Denis Plotnikov <denis.plotnikov@exactprosystems.com>
2022-11-28 13:48:12 +0400

    introduce registry mapping and layers mapping

--- extract.py
+++ extract.py
@@ -22,24 +22,15 @@ import tarfile
 import threading
 from typing import List
 
-CONFIG_FILE_NAME: str = "images_to_load.json"
+CONFIG_FILE_NAME: str = "extract_config.json"
 SCRIPT_FILE_NAME: str = "docker_extractor.sh"
+
 RUN_SCRIPT_POSTFIX: str = os.sep + "bin" + os.sep + "service"
 SERVICE_DIR_POSTFIX: str = os.sep + "home" + os.sep + "service"
 HOME_DIR_POSTFIX: str = os.sep + "home" + os.sep
 LIB_DIR: str = os.sep + "components" + os.sep
 HOME_IGNORE: List[str] = ["service", "Dockerfile"]
 MAIN_CLASS_LINE_INDICATOR: str = "eval set --"
-EXTRA_LAYERS_IMAGES: List[str] = [
-    'codec-fix',
-    'codec-ntg',
-    'codec-fast',
-    'codec-itch',
-    'codec-soup',
-    'conn-fix',
-    'conn-ntg',
-    'conn-soup'
-]
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
@@ -48,13 +39,16 @@ def make_tarfile(output_filename, source_dir):
             tar.add(p, arcname=file)
 
 class Config(object):
-    def __init__(self, images):
+    def __init__(self, images, layers_config, registry_mapping):
         self.images = images
+        self.layers_config = layers_config
+        self.registry_mapping = registry_mapping
 
 class ImageExtractor(object):
-    def __init__(self, image_name: str, cwd: str):
+    def __init__(self, image_name: str, cwd: str, config: Config):
         self.image_name = image_name
         self.cwd = cwd
+        self.config = config
 
     def extract_and_prepare(self):
         out_dir = self.__call_script()
@@ -63,17 +57,25 @@ class ImageExtractor(object):
 
     def __call_script(self) -> str:
         number_of_layers = 1
-        for image_part in EXTRA_LAYERS_IMAGES:
-            if(self.image_name.__contains__(image_part)):
-                number_of_layers = 4
+        for (key, value) in self.config.layers_config.items():
+            if(self.image_name.__contains__(key)):
+                number_of_layers = value
+
+        domain = self.image_name.split("/")[0]
+        is_mapping_presented = domain in self.config.registry_mapping
+
+        auth_type = "token"
+        if(is_mapping_presented):
+            auth_type = "basic"
+            [user, password] = self.config.registry_mapping[domain].split(":")
+            os.environ["BASIC_USER"] = user
+            os.environ["BASIC_PASSWORD"] = password
+
         out_dir = self.cwd + LIB_DIR + os.sep + self.image_name.split("/")[-1]
-        domain = "ghcr"
-        if(self.image_name.__contains__("nexus")):
-            domain = "nexus"
         args_list = [
-            '-n', str(number_of_layers), 
+            '-n', str(number_of_layers),
             '-o', out_dir,
-            '-t', domain,
+            '-t', auth_type,
             self.image_name
         ]
         print("Started extraction of image {0}".format(self.image_name))
@@ -83,7 +85,7 @@ class ImageExtractor(object):
         if(execution_result.returncode != 0 or not os.path.exists(out_dir)):
             sys.exit(1)
         return out_dir
-    
+
     def __prepare_directory(self, dir):
         cwd = dir
         home_dir = cwd + HOME_DIR_POSTFIX
@@ -91,7 +93,7 @@ class ImageExtractor(object):
             if(filename in HOME_IGNORE):
                 continue
             shutil.move(home_dir + os.sep + filename, cwd)
-        
+
         service_dir = cwd + SERVICE_DIR_POSTFIX
         for filename in os.listdir(service_dir):
             shutil.move(service_dir + os.sep + filename, cwd)
@@ -101,22 +103,23 @@ class ImageExtractor(object):
     def __extract_main_class(self, run_file, cwd):
         with open(run_file) as file:
             with open(cwd + os.sep + "mainclass", "a") as result:
-                for line in file:   
+                for line in file:
                     if(line.__contains__(MAIN_CLASS_LINE_INDICATOR)):
                         result.write(line.rstrip().split(" ")[-2])
 
 
-        
+
 
 if __name__ == "__main__":
     config_file = open(CONFIG_FILE_NAME)
     j = json.load(config_file)
     config_file.close()
+
     config = Config(**j)
     cwd = os.getcwd()
     threads: List[threading.Thread] = []
     for image in config.images:
-        extractor = ImageExtractor(image, cwd)
+        extractor = ImageExtractor(image, cwd, config)
         threads.append(threading.Thread(target=extractor.extract_and_prepare))
 
     for x in threads:
